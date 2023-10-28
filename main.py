3e24f22d5fcc7f731fef644412fa04da1af4e12d
HamidrezaDeh <hamiddeh19@gmail.com>
2023-10-29 00:28:56 +0330

    add operator 'ADD'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 
 state = 'MAIN'
+calc_value = 0
 
 
 def show_main_menu():
@@ -15,36 +16,58 @@ def show_main_menu():
 def show_calc_menu():
     table = []
     headers = ["ID", "Command"]
+    table.append(["1"] + ["ADD"])
+    #table.append(["2"] + ["MULTIPLY"])
+    #table.append(["3"] + ["DIVIDE"])
+
     table.append(["0"] + ["BACK"])
-    print()
+    print("Current Value:", calc_value)
     print(tabulate(table, headers=headers))
 
 
 def show_menu():
     if state == 'MAIN':
         show_main_menu()
-    if state == 'CALCULATION':
+    elif state == 'CALCULATION':
         show_calc_menu()
 
 
 def handle_input_main(x):
     if x == '0':
         exit()
-    if x == '1':
+    elif x == '1':
         global state
         state = 'CALCULATION'
+
+
+def get_numbers():
+    nums = []
+    next_num = input()
+    while next_num != '':
+        nums.append(float(next_num))
+        next_num = input()
+    return nums
+
+
+def add(nums):
+    for num in nums:
+        global calc_value
+        calc_value += num
 
 
 def handle_input_calc(x):
     if x == '0':
         global state
         state = 'MAIN'
+    elif x == '1':
+        nums = get_numbers()
+        add(nums)
 
 
 def handle_input(x):
     if state == 'MAIN':
         handle_input_main(x)
-    if state == 'CALCULATION':
+    elif state == 'CALCULATION':
         handle_input_calc(x)
 
 
