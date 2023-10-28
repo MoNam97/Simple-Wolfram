8db7375a7e92f689af3b746ad0a969bbe9483218
MoNam97 <mohamad0namdar@gmail.com>
2023-10-29 00:18:08 +0330

    implement ADD function and handler + add UI for ADD function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tabulate import tabulate
 
 state = 'MAIN'
-
+calculation_value = 0
 
 def show_main_menu():
     table = []
@@ -12,15 +12,39 @@ def show_main_menu():
     print(tabulate(table, headers=headers))
 
 
+def add_func(x: int, y: list):
+    for number in y:
+        x += number
+    return x
+
+def add_handler():
+    print("Enter the list of numbers separated by ',' to be added to the current value:")
+    list_of_numbers = input()
+    list_of_numbers = list_of_numbers.replace(" ", "")
+    list_of_numbers = list_of_numbers.split(",")
+    for i in range(len(list_of_numbers)):
+        if list_of_numbers[i].isdigit():
+            list_of_numbers[i] = int(list_of_numbers[i])
+        elif list_of_numbers[i].replace(".", "", 1).isdigit():
+            list_of_numbers[i] = float(list_of_numbers[i])
+        else:
+            print("Invalid input")
+            return
+    print()
+    global calculation_value
+    calculation_value = add_func(calculation_value, list_of_numbers)
+
 def show_calc_menu():
-    calculation_value = 0
+    global calculation_value
     table = []
     headers = ["ID", "Command"]
+    table.append(["1"] + ["Add"])
+    # table.append("              ")
     table.append(["0"] + ["BACK"])
     print("===============================")
     print("\tCALCULATION MENU")
     print()
-    print("Current Value: ", calculation_value)
+    print("Current Value:", calculation_value)
     print()
     print(tabulate(table, headers=headers))
 
@@ -44,12 +68,15 @@ def handle_input_calc(x):
     if x == '0':
         global state
         state = 'MAIN'
+    elif x == '1':
+        add_handler()
+        
 
 
 def handle_input(x):
     if state == 'MAIN':
         handle_input_main(x)
-    if state == 'CALCULATION':
+    elif state == 'CALCULATION':
         handle_input_calc(x)
 
 
