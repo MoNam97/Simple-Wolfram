611a2421f8c075fea767dd7cf6676d8f552f1ef6
MoNam97 <mohamad0namdar@gmail.com>
2023-10-29 00:49:41 +0330

    implement POWER function and handler + add UI for POWER function

--- main.py
+++ main.py
@@ -59,12 +59,36 @@ def devide_handler():
     calculation_value = devide_func(calculation_value, list_of_numbers)
 
 
+def power_func(x: int, y: list):
+    for number in y:
+        x **= number
+    return x
+
+def power_handler():
+    print("Enter the list of numbers separated by ',' that current value will be powered by:")
+    list_of_numbers = input()
+    list_of_numbers = list_of_numbers.replace(" ", "")
+    list_of_numbers = list_of_numbers.split(",")
+    for i in range(len(list_of_numbers)):
+        if list_of_numbers[i].isdigit():
+            list_of_numbers[i] = int(list_of_numbers[i])
+        elif list_of_numbers[i].replace(".", "", 1).isdigit():
+            list_of_numbers[i] = float(list_of_numbers[i])
+        else:
+            print("Invalid input")
+            return
+    print()
+    global calculation_value
+    calculation_value = power_func(calculation_value, list_of_numbers)
+
+
 def show_calc_menu():
     global calculation_value
     table = []
     headers = ["ID", "Command"]
     table.append(["1"] + ["Add"])
     table.append(["4"] + ["Devide"])
+    table.append(["5"] + ["Power"])
     # table.append("              ")
     table.append(["0"] + ["BACK"])
     print("===============================")
@@ -98,6 +122,8 @@ def handle_input_calc(x):
         add_handler()
     elif x == '4':
         devide_handler()
+    elif x == '5':
+        power_handler()
         
 
 
