cda18548275ccb4fe61245354e922c38ed021879
HamidrezaDeh <hamiddeh19@gmail.com>
2023-10-29 21:01:19 +0330

    resolve conflicts :)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ calc_value = 0
 # [ ] TODO: Debug the UI concerning empty lines
 # [ ] TODO: make the input taking format consistent
 
+
 def show_main_menu():
     table = []
     headers = ["ID", "Command"]
@@ -16,76 +17,19 @@ def show_main_menu():
     print(tabulate(table, headers=headers))
 
 
-def calc_input_parser(list_of_numbers):
-    list_of_numbers = list_of_numbers.replace(" ", "")
-    list_of_numbers = list_of_numbers.split(",")
-    for i in range(len(list_of_numbers)):
-        if list_of_numbers[i].isdigit():
-            list_of_numbers[i] = int(list_of_numbers[i])
-        elif list_of_numbers[i].replace(".", "", 1).isdigit():
-            list_of_numbers[i] = float(list_of_numbers[i])
-        else:
-            print("Invalid input")
-            return
-    return list_of_numbers
-
-
-def add_func(x: int, y: list):
-    for number in y:
-        x += number
-    return x
-
-def add_handler():
-    print("Enter the list of numbers separated by ',' to be added to the current value:")
-    list_of_numbers = calc_input_parser(input())
-    print()
-    global calculation_value
-    calculation_value = add_func(calculation_value, list_of_numbers)
-
-
-def devide_func(x: int, y: list):
-    for number in y:
-        x /= number
-    return round(x, 8)
-# rounded because of floating point error in binary system. i.e. 12/5/3 = 0.7999999999999999
-
-def devide_handler():
-    print("Enter the list of numbers separated by ',' that current value will be divided by:")
-    list_of_numbers = calc_input_parser(input())
-    print()
-    global calculation_value
-    calculation_value = devide_func(calculation_value, list_of_numbers)
-
-
-def power_func(x: int, y: list):
+def power(x: int, y: list):
     for number in y:
         x **= number
     return x
 
-def power_handler():
-    print("Enter the list of numbers separated by ',' that current value will be powered by:")
-    list_of_numbers = calc_input_parser(input())
-    print()
-    global calculation_value
-    calculation_value = power_func(calculation_value, list_of_numbers)
 
-
-def root_func(x: int, y: list):
+def root(x: int, y: list):
     for number in y:
         x **= 1/number
     return x
-    # return round(x, 8)
-
-def root_handler():
-    print("Enter the list of numbers separated by ',' that current value will be rooted by:")
-    list_of_numbers = calc_input_parser(input())
-    print()
-    global calculation_value
-    calculation_value = root_func(calculation_value, list_of_numbers)
 
 
 def show_calc_menu():
-    global calculation_value
     table = []
     headers = ["ID", "Command"]
     table.append(["1"] + ["ADD"])
@@ -94,11 +38,11 @@ def show_calc_menu():
     table.append(["4"] + ["DIVIDE"])
     table.append(["5"] + ["POWER"])
     table.append(["6"] + ["ROOT"])
-    # table.append([""] + [""])
-    # table.append(["R"] + ["RESET"])
+    table.append([""] + [""])
+    table.append(["R"] + ["RESET"])
     table.append(["0"] + ["BACK"])
     print("===================================")
-    print("\tCalculation Menu")
+    print("Calculation Menu:")
     print()
     print("Current Value:", calc_value)
     print(tabulate(table, headers=headers))
@@ -158,11 +102,13 @@ def reset():
 
 
 def handle_input_calc(x):
+    global calc_value
     if x == '0':
         global state
         state = 'MAIN'
     elif x == '1':
-        add_handler()
+        nums = get_numbers()
+        add(nums)
     elif x == '2':
         nums = get_numbers()
         subtract(nums)
@@ -170,11 +116,14 @@ def handle_input_calc(x):
         nums = get_numbers()
         multiply(nums)
     elif x == '4':
-        devide_handler()
+        nums = get_numbers()
+        divide(nums)
     elif x == '5':
-        power_handler()
+        nums = get_numbers()
+        calc_value = power(calc_value, nums)
     elif x == '6':
-        root_handler()
+        nums = get_numbers()
+        calc_value = root(calc_value, nums)
     elif x == 'R':
         reset()
 
